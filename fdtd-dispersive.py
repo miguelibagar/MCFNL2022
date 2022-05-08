a123af2a9a5c5ee4497af07fb720b97190d72c04
Miguel Ibáñez <miguelibaezgarcia@gmail.com>
2022-05-08 17:25:15 +0200

    fallos en las ecuaciones

--- fdtd-dispersive.py
+++ fdtd-dispersive.py
@@ -6,15 +6,16 @@ import matplotlib.pyplot as plt
 #mu = 4*pi*1e-7
 #eps0 = 8.8541878128e-12
 # Voy a trabajar en unidades naturales
-mu = 1e-7
-c = 100
+mu = 1
+eps = 1
+c = 1
 
 ## - Parametrizacion de la permitividad
 cVec = np.array([5.987e-1 +4.195e3j,-2.211e-1+2.680e-1j,-4.240+7.324e2j])
 aVec = np.array([-2.502e-2-8.626e-3j,-2.021-9.407e-1j,-1.467e1-1.338j])
 
 ## -- Grid
-tEnd = 1.0
+tEnd = 2.0
 
 xIni = 0
 xEnd = 10
@@ -37,28 +38,30 @@ Dx = grid[1] - grid[0]
 Dt = 0.8*Dx/c # Para que sea dimensionalmente correcto
 
 ## -- Parametros necesarios para resolver
-kVec = 2 / (1-0.5*Dt*aVec)
-bVec = 8.85e-12*Dt*(cVec / (1-0.5*Dt*aVec))
+kVec = (1+0.5*Dt*aVec) / (1-0.5*Dt*aVec)
+k1Vec = 2 / (1-0.5*Dt*aVec)
+bVec = eps*Dt*(cVec / (1-0.5*Dt*aVec))
 
 ## -- Algoritmo
 # Primero inicializamos
-Eold = Eini
-Jold = np.zeros([N,len(cVec)])
-Hold = dualGrid*0.0
+Eold = Eini*(1+0j)
+Jold = np.zeros([N,len(cVec)])*(0+0j)
+Hold = dualGrid*(0+0j)
 
-Enew = Eold*0.0
-Jnew = Jold*0.0
-Hnew = Hold*0.0
+Enew = Eold*(0+0j)
+Jnew = Jold*(0+0j)
+Hnew = Hold*(0+0j)
 
 # Aqui pongo condiciones de contorno de campo nulo
 # El campo electrico tiene la cosa de que llega hasta los extremos
 Enew[0] = 0.0
 Enew[-1] = 0.0
 
+suma2 = np.sum(np.real(bVec))
+
 t=0.0
 while t < tEnd:
-    suma1 = np.real(np.sum(Jold*kVec,axis=1))
-    suma2 = np.sum(np.real(bVec))
+    suma1 = np.real(np.sum(Jold*k1Vec,axis=1))
     
     Enew[1:-1] = Eold[1:-1] + (Dt/suma2)*((Hold[1:] - Hold[:-1])/Dx - suma1[1:-1])
     
@@ -68,10 +71,11 @@ while t < tEnd:
     Hnew[:] = -(Dt/(mu*Dx)) * (Enew[1:] - Enew[:-1]) + Hold[:]
     
     Eold[:] = Enew[:]
+    Jold[:] = Jnew[:]
     Hold[:] = Hnew[:]
     
-    Eold[0] = 0.0
-    Eold[-1] = 0.0
+    Eold[0] = 0+0j
+    Eold[-1] = 0+0j
     
     t+=Dt
 
@@ -80,7 +84,8 @@ while t < tEnd:
 # Luego grid[:-1] coge desde el primero hasta el penultimo y
 # grid[1:] coge desde el segundo hasta el ultimo
 
-plt.plot(grid,Enew)
-plt.plot(dualGrid,Hnew)
+plt.figure()
+plt.plot(grid,np.real(Enew))
+plt.plot(dualGrid,np.real(Hnew))
 plt.grid()
 plt.show()
