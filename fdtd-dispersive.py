8728fc822a58f326e9250e218e44e0c4df699e62
Miguel Ibáñez <miguelibaezgarcia@gmail.com>
2022-05-09 19:54:57 +0200

    prueba isaac

--- fdtd-dispersive.py
+++ fdtd-dispersive.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 #mu = 4*pi*1e-7
@@ -10,21 +11,24 @@ eps = 1
 c = 1
 
 ## - Parametrizacion de la permitividad
-cVec = np.array([5.987e-1 +4.195e3j,-2.211e-1+2.680e-1j,-4.240+7.324e2j])
-aVec = np.array([-2.502e-2-8.626e-3j,-2.021-9.407e-1j,-1.467e1-1.338j])
+# cVec = np.array([5.987e-1 +4.195e3j,-2.211e-1+2.680e-1j,-4.240+7.324e2j])
+#aVec = np.array([-2.502e-2-8.626e-3j,-2.021-9.407e-1j,-1.467e1-1.338j])
+aVec = np.array([-20-30j,-20-10j,-10-10j])*1e-1
+
+cVec = np.array([-10-30j,-20-10j,20-10j])*1e4
 
 #cVec = np.array([5.987e-1 +4.195e3j])
 #aVec = np.array([-2.502e-2-8.626e-3j])
 
 ## -- Grid
-tEnd = 5
+tEnd = 40
 
 xIni = 0
 xEnd = 10
 N = int(101)
 
 grid = np.linspace(xIni,xEnd,N)
-dualGrid = (grid[:-1] + grid[1:])/2.0 # Tiene un punto menos que el primal
+dualGrid = (grid[:-1] + grid[1:])/5.0 # Tiene un punto menos que el primal
 
 ## - Condicion inicial (gaussiana)
 media = (xIni + xEnd)/2.0
@@ -37,9 +41,10 @@ plt.show()
 
 ## -- Espaciados
 Dx = grid[1] - grid[0]
-Dt = 0.8*Dx/c # Para que sea dimensionalmente correcto
+Dt = 0.80*Dx/c # Para que sea dimensionalmente correcto
 
 ## -- Parametros necesarios para resolver
+vTest = 1-0.5*Dt*aVec
 kVec = (1+0.5*Dt*aVec) / (1-0.5*Dt*aVec)
 k1Vec = 2 / (1-0.5*Dt*aVec)
 bVec = eps*Dt*(cVec / (1-0.5*Dt*aVec))
@@ -54,18 +59,24 @@ Enew = Eold*(0+0j)
 Jnew = Jold*(0+0j)
 Hnew = Hold*(0+0j)
 
-suma2 = 100+ np.sum(np.real(bVec))
 
-limite = 80
+Edata, Hdata = [],[]
+fig = plt.figure(figsize = (15,15))
+ax = fig.add_subplot(111)
+
+suma2 = 0 + np.sum(np.real(bVec))
+
+limite = 40
 
 t=0.0
 while t < tEnd:
     suma1 = np.real(np.sum(Jold*k1Vec,axis=1))
             
-    Enew[1:limite] = Eold[1:limite] - (Dt/(eps*Dx))*(Hold[1:limite]-Hold[0:(limite-1)])
+    #Enew[1:limite+1] = Eold[1:limite+1] - (Dt/(eps*Dx))*(Hold[1:limite+1]-Hold[0:(limite)])
     #Enew[1:3] = Eold[1:3] - (Dt/(eps*Dx))*(Hold[1:3]-Hold[0:2])
     
-    Enew[limite:-1] = Eold[limite:-1] + (Dt/suma2)*((Hold[limite:] - Hold[(limite-1):-1])/Dx - suma1[limite:-1])
+    Enew[1:-1] = Eold[1:-1] + (Dt/suma2)*((Hold[1:] - Hold[:-1])/Dx - suma1[1:-1])
+    #Enew[limite:-1] = Eold[limite:-1] + (Dt/suma2)*((Hold[limite:] - Hold[(limite-1):-1])/Dx - suma1[limite:-1])
     #Enew[3:-1] = Eold[3:-1] + (Dt/suma2)*((Hold[3:] - Hold[2:-1])/Dx - suma1[3:-1])
     
     for p in range(len(cVec)):
@@ -73,12 +84,20 @@ while t < tEnd:
     
     Hnew[:] = -(Dt/(mu*Dx)) * (Enew[1:] - Enew[:-1]) + Hold[:]
     
-    Eold[:] = Enew[:]
-    Jold[:] = Jnew[:]
-    Hold[:] = Hnew[:]
+    EnewR = np.real(Enew)
+    HnewR = np.real(Hnew)
+    
+    Edata.append(EnewR.copy())
+    Hdata.append(HnewR.copy())
+    
+    Eold = Enew[:]
+    Jold = Jnew[:]
+    Hold = Hnew[:]
     
     Eold[0] = 0+0j
     Eold[-1] = 0+0j
+    
+    
     
     t+=Dt
 
@@ -87,13 +106,45 @@ while t < tEnd:
 # Luego grid[:-1] coge desde el primero hasta el penultimo y
 # grid[1:] coge desde el segundo hasta el ultimo
 
-plt.close()
-plt.figure()
-plt.plot(grid,np.real(Enew))
-plt.plot(dualGrid,np.real(Hnew))
-plt.legend(['Electrico','Magnetico'])
-plt.xlim(0,11)
-plt.ylim(-3,3)
-rectangle = plt.Rectangle((limite/xEnd,-3.0),4,6,fc='blue',ec="blue")
-plt.gca().add_patch(rectangle)
-plt.grid()
+# plt.close()
+# plt.figure()
+# plt.plot(grid,np.real(Enew))
+# plt.plot(dualGrid,np.real(Hnew))
+# plt.legend(['Electrico','Magnetico'])
+# #plt.xlim(0,11)
+# #plt.ylim(-3,3)
+# rectangle = plt.Rectangle((limite/xEnd,-3.0),4,6,fc='blue',ec="blue")
+# plt.gca().add_patch(rectangle)
+# plt.grid()
+
+
+
+
+
+def animate(i):
+    ax.clear()
+    ax.set(xlim=(0, 10), ylim=(-1.5, 1.5))
+
+    ax.plot(grid,Edata[i],'o' ,color = 'darkblue',markersize=2)
+    ax.plot(dualGrid,Hdata[i],'o',color = 'red',markersize=2)
+    rectangle = plt.Rectangle((8,-10), 2, 40, fc='blue',ec="blue")
+    plt.gca().add_patch(rectangle)
+    plt.grid(True)
+    
+
+
+
+
+anim = animation.FuncAnimation(fig, animate,frames=len(Edata),interval=20,blit=False)
+#anim.save('caca.mp4', writer = 'ffmpeg', fps = 30)
+
+
+#anim.save('basic_animation.gif', fps=30, extra_args=['-vcodec', 'libx264'])
+#anim.save('caca.gif')
+                            
+plt.show()
+
+# grid[a:b] coge el intervalo [a,b). El -1 es el ultimo (empiezo en 0)
+# Luego grid[:-1] coge desde el primero hasta el penultimo y
+# grid[1:] coge desde el segundo hasta el ultimo
+
