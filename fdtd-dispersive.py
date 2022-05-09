f34ca9c7c08c013ae270f241df826ae34e316d17
Miguel Ibáñez <miguelibaezgarcia@gmail.com>
2022-05-09 20:47:02 +0200

    milagros

--- fdtd-dispersive.py
+++ fdtd-dispersive.py
@@ -21,23 +21,23 @@ cVec = np.array([-10-30j,-20-10j,20-10j])*1e4
 #aVec = np.array([-2.502e-2-8.626e-3j])
 
 ## -- Grid
-tEnd = 40
+tEnd = 50
 
 xIni = 0
 xEnd = 10
-N = int(101)
+N = int(201)
 
 grid = np.linspace(xIni,xEnd,N)
 dualGrid = (grid[:-1] + grid[1:])/2.0 # Tiene un punto menos que el primal
 
 ## - Condicion inicial (gaussiana)
-media = (xIni + xEnd)/3.0
+media = (xIni+xEnd)/3.0
 sigma= (xIni-xEnd)/50.0
 
 Eini = np.exp( - np.power(grid - media,2) / (2.0*sigma**2))
 
-plt.plot(grid,Eini)
-plt.show()
+# plt.plot(grid,Eini)
+# plt.show()
 
 ## -- Espaciados
 Dx = grid[1] - grid[0]
@@ -66,7 +66,11 @@ ax = fig.add_subplot(111)
 
 suma2 = 200 + np.sum(np.real(bVec))
 
-limite = 40
+x1 = 2.0
+x2 = 4.5
+
+limite1 = int(x1/Dx)
+limite2 = int(x2/Dx)
 
 acum = 0
 suma1 = np.zeros(N)*(0+0j)
@@ -79,11 +83,14 @@ while t < tEnd:
         suma1[i] = np.real(acum)
         acum = 0
 
-    #Enew[1:limite+1] = Eold[1:limite+1] - (Dt/(eps*Dx))*(Hold[1:limite+1]-Hold[0:(limite)])
+    Enew[1:limite1+1] = Eold[1:limite1+1] - (Dt/(eps*Dx))*(Hold[1:limite1+1]-Hold[0:(limite1)])
     #Enew[1:3] = Eold[1:3] - (Dt/(eps*Dx))*(Hold[1:3]-Hold[0:2])
     
-    Enew[1:-1] = Eold[1:-1] + (Dt/suma2)*((Hold[1:] - Hold[:-1])/Dx - suma1[1:-1])
-    #Enew[limite:-1] = Eold[limite:-1] + (Dt/suma2)*((Hold[limite:] - Hold[(limite-1):-1])/Dx - suma1[limite:-1])
+    #Enew[1:-1] = Eold[1:-1] + (Dt/suma2)*((Hold[1:] - Hold[:-1])/Dx - suma1[1:-1])
+    Enew[limite1:limite2+1] = Eold[limite1:limite2+1] + (Dt/suma2)*((Hold[limite1:limite2+1] - Hold[(limite1-1):limite2])/Dx - suma1[limite1:limite2+1])
+    
+    Enew[limite2:-1] = Eold[limite2:-1] - (Dt/(eps*Dx))*(Hold[limite2:]-Hold[(limite2-1):-1])
+    
     #Enew[3:-1] = Eold[3:-1] + (Dt/suma2)*((Hold[3:] - Hold[2:-1])/Dx - suma1[3:-1])
     
     for p in range(len(cVec)):
@@ -118,9 +125,9 @@ plt.figure()
 plt.plot(grid,np.real(Enew))
 plt.plot(dualGrid,np.real(Hnew))
 plt.legend(['Electrico','Magnetico'])
-#plt.xlim(0,11)
-#plt.ylim(-3,3)
-rectangle = plt.Rectangle((limite/xEnd,-3.0),4,6,fc='blue',ec="blue")
+plt.xlim(0,11)
+plt.ylim(-5,5)
+rectangle = plt.Rectangle((x1,-5), (x2-x1), 10, fc='blue',ec="blue")
 plt.gca().add_patch(rectangle)
 plt.grid()
 
@@ -130,11 +137,11 @@ plt.grid()
 
 def animate(i):
     ax.clear()
-    ax.set(xlim=(0, 10), ylim=(-10, 10))
+    ax.set(xlim=(0, 10), ylim=(-5, 5))
 
     ax.plot(grid,Edata[i],'o' ,color = 'darkblue',markersize=2)
     ax.plot(dualGrid,Hdata[i],'o',color = 'red',markersize=2)
-    rectangle = plt.Rectangle((8,-10), 2, 40, fc='blue',ec="blue")
+    rectangle = plt.Rectangle((x1,-5), (x2-x1), 10, fc='blue',ec="blue")
     plt.gca().add_patch(rectangle)
     plt.grid(True)
     
